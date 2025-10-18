c9782c47b9b1e0c8d0e4f4b33c0faec95e42cce2
Dalami <diegoalami@gmail.com>
2025-10-18 11:31:24 -0300

    Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  define: {
+    'process.env': process.env
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://magnetico-server-1.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
